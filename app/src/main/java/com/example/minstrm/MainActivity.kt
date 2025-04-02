904eec871d38dedb94bbae21a09b93dc117c65f6
mathiasHC99 <MathiasHC@live.dk>
2025-04-02 11:01:51 +0200

    test

--- app/src/main/java/com/example/minstrm/MainActivity.kt
+++ app/src/main/java/com/example/minstrm/MainActivity.kt
@@ -4,13 +4,22 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.minstrm.ui.theme.MinStrømTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,28 +29,33 @@ class MainActivity : ComponentActivity() {
         setContent {
             MinStrømTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
+                    GreetingCard(
                         name = "Android",
+                        onButtonClick = { /* do something */ },
                         modifier = Modifier.padding(innerPadding)
                     )
+
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    MinStrømTheme {
-        Greeting("Android")
+fun GreetingCard(name: String, onButtonClick: () -> Unit) {
+    Card(
+        modifier = Modifier
+            .padding(16.dp)
+            .fillMaxWidth(),
+        elevation = CardDefaults.cardElevation(8.dp)
+    ) {
+        Column(modifier = Modifier.padding(16.dp)) {
+            Text(text = "Hej, $name!", style = MaterialTheme.typography.titleLarge)
+            Spacer(modifier = Modifier.height(8.dp))
+            Button(onClick = onButtonClick) {
+                Text("Klik mig")
+            }
+        }
     }
 }
